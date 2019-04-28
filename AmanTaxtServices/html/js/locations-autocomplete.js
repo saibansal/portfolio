c156a4cba29a3e49faa48a798ca41cac67baebb4
saibansal <saibansal87@gmail.com>
2019-04-28 18:07:18 +0530

    editing html of aman taxi service

--- AmanTaxtServices/html/js/locations-autocomplete.js
+++ AmanTaxtServices/html/js/locations-autocomplete.js
@@ -1,9 +1,9 @@
 var locations = [
-{ value: "Santa Monica - 2102 Lincoln Blvd"},
-{ value: "With Co-Ordinates  - 5711 W Century Blvd", latLng: [33.946272, -118.381641]},
-{ value: "Las Vegas - 6401 Centennial Center Blvd"},
-{ value: "New York - 610 Warren St"},
-{ value: "Orlando - 2125 W Landstreet Rd"},
-{ value: "Washington - 50 Massachusetts Ave NE"},
-{ value: "Denver - 7800 E Tufts Ave"}
+//{ value: "Santa Monica - 2102 Lincoln Blvd"},
+{ value: "Taxi Stand Sec 49 C Chandigarh. 160047.<br><br> Cont :  Sr. Kulwant Singh <br>9872900067, 7837700067 ", latLng: [30.693503, 76.752889]},
+//{ value: "Las Vegas - 6401 Centennial Center Blvd"},
+//{ value: "New York - 610 Warren St"},
+//{ value: "Orlando - 2125 W Landstreet Rd"},
+//{ value: "Washington - 50 Massachusetts Ave NE"},
+//{ value: "Denver - 7800 E Tufts Ave"}
 ];
